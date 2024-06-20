b15ee8d9f956db36076327c3157080aa11ed1bee
margaridacavadas <margarida.g.cavadas@gmail.com>
2024-06-20 16:52:41 +0100

    add structure for red album lyrics

---
+++ src/red-lyrics.js
@@ -0,0 +1,137 @@
+var stateOfGrace = {
+    albumId: 4,
+    song: "State Of Grace",
+    lyrics:
+};
+
+var red = {
+    albumId: 4,
+    song: "Red",
+    lyrics:
+};
+
+var treacherous = {
+    albumId: 4,
+    song: "Treacherous",
+    lyrics:
+};
+
+var iKnewYouWereTrouble = {
+    albumId: 4,
+    song: "I Knew You Were Trouble",
+    lyrics:
+};
+
+var allTooWell = {
+    albumId: 4,
+    song: "All Too Well",
+    lyrics:
+};
+
+var twentyTwo = {
+    albumId: 4,
+    song: "22",
+    lyrics:
+};
+
+var iAlmostDo = {
+    albumId: 4,
+    song: "I Almost Do",
+    lyrics:
+};
+
+var weAreNeverGettingBackTogether = {
+    albumId: 4,
+    song: "We Are Never Getting Back Together",
+    lyrics:
+};
+
+var stayStayStay = {
+    albumId: 4,
+    song: "Stay Stay Stay",
+    lyrics:
+};
+
+var theLastTime = {
+    albumId: 4,
+    song: "The Last Time",
+    lyrics:
+};
+
+var holyGround = {
+    albumId: 4,
+    song: "Holy Ground",
+    lyrics:
+};
+
+var sadBeautifulTragic = {
+    albumId: 4,
+    song: "Sad Beautiful Tragic",
+    lyrics:
+};
+
+var theLuckyOne = {
+    albumId: 4,
+    song: "The Lucky One",
+    lyrics:
+};
+
+var everythingHasChanged = {
+    albumId: 4,
+    song: "Everything Has Changed",
+    lyrics:
+};
+
+var starlight = {
+    albumId: 4,
+    song: "Starlight",
+    lyrics:
+};
+
+var beginAgain = {
+    albumId: 4,
+    song: "Begin Again",
+    lyrics:
+};
+
+var theMomentIKnew = {
+    albumId: 4,
+    song: "The Moment I Knew",
+    lyrics:
+};
+
+var comeBackBeHere = {
+    albumId: 4,
+    song: "Come Back... Be Here",
+    lyrics:
+};
+
+var girlAtHome = {
+    albumId: 4,
+    song: "Girl At Home",
+    lyrics:
+};
+
+var redAlbum = [
+    stateOfGrace,
+    red,
+    treacherous,
+    iKnewYouWereTrouble,
+    allTooWell,
+    twentyTwo,
+    iAlmostDo,
+    weAreNeverGettingBackTogether,
+    stayStayStay,
+    theLastTime,
+    holyGround,
+    sadBeautifulTragic,
+    theLuckyOne,
+    everythingHasChanged,
+    starlight,
+    beginAgain,
+    theMomentIKnew,
+    comeBackBeHere,
+    girlAtHome
+];
+
+export{redAlbum};
